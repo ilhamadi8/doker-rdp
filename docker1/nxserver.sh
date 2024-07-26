758a4b7b84e18fa65daddb25ee90b3b3f9fc26aa
ilhamadi8 <156057693+ilhamadi8@users.noreply.github.com>
2024-07-26 18:32:30 +0000

    update

--- docker1/nxserver.sh
+++ docker1/nxserver.sh
@@ -7,4 +7,9 @@ groupadd -r $USER -g 433 \
 && echo $USER':'$PASSWORD | chpasswd
 /etc/NX/nxserver --startup
 service xrdp start
+echo "exec /usr/bin/xfce4-session" > ~/.chrome-remote-desktop-session
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AcvDMrDWd4M_HqNrcNmcaF50oSMMI8usZgIUyVBrHqxfVvNAPgqf3cwm4t4XeWHKkgnDwQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
+service chrome-remote-desktop enable
+service chrome-remote-desktop start
 tail -f /usr/NX/var/log/nxserver.log
+
